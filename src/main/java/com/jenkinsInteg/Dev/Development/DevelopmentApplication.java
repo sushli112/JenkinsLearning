3835cddaee6b0faf9ecffd04ca33b96fe5b5a747
unknown <sushilkumar4sep@gmail.com>
2018-10-14 17:16:10 +0530

    first commit

---
+++ src/main/java/com/jenkinsInteg/Dev/Development/DevelopmentApplication.java
@@ -0,0 +1,19 @@
+package com.jenkinsInteg.Dev.Development;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DevelopmentApplication {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(DevelopmentApplication.class);
+		logger.debug("Inside DevelopmentApplication");
+		SpringApplication.run(DevelopmentApplication.class, args);
+
+
+	}
+}
